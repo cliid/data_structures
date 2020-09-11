a64db8ad0dd6774d429673fd33a2ed7121fdb34e
cliid <jiwujang@naver.com>
2020-09-11 14:07:30 +0900

    Linked List Added

---
+++ linked_list/list.h
@@ -0,0 +1,92 @@
+#ifndef LIST_H
+#define LIST_H
+
+class List
+{
+private:
+    struct node
+    {
+        int data;
+        node *next;
+    };
+
+    typedef struct node *nodePtr;
+
+    nodePtr head, curr, temp;
+
+public:
+    List(); // constructor
+    void AddNode(int addData);
+    void DeleteNode(int delData);
+    void PrintList();
+};
+
+List::List()
+{
+    head = nullptr;
+    curr = nullptr;
+    temp = nullptr;
+}
+
+void List::AddNode(int addData)
+{
+    nodePtr n;
+    n->next = nullptr;
+    n->data = addData;
+
+    if (head != nullptr)
+    {
+        curr = head;
+        while (curr->next != nullptr)
+        {
+            curr = curr->next;
+        }
+        curr->next = n;
+    }
+    else
+    {
+        head = n;
+    }
+}
+
+void List::DeleteNode(int delData)
+{
+    nodePtr delPtr = nullptr;
+    temp = head;
+    curr = head;
+    while (curr != nullptr && curr->data != delData)
+    {
+        temp = curr;
+        curr = curr->next;
+    }
+    if (curr == nullptr)
+    {
+        std::cout << delData << "was not in the list." << std::endl;
+        delete delPtr;
+    }
+    else
+    {
+        delPtr = curr;
+        curr = curr->next;
+        temp->next = curr;
+        if (delPtr == head)
+        {
+            head = head->next;
+            temp = nullptr;
+        }
+        delete delPtr;
+        std::cout << "The value " << delData << " was deleted." << std::endl;
+    }
+}
+
+void List::PrintList()
+{
+    curr = head;
+    while (curr != nullptr)
+    {
+        std::cout << curr->data << std::endl;
+        curr = curr->next;
+    }
+}
+
+#endif /* LIST_H */
---
+++ linked_list/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "list.h"
+
+int main(int argc, char const *argv[])
+{
+    List paul;
+    paul.AddNode(3);
+    return 0;
+}
